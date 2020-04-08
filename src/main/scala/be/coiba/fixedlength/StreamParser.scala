0cc0473c901c27a637de0b20d3edc2c793a6f9eb
Jonas Heylen <jonas.heylen@ing.com>
2020-04-08 13:38:28 +0200

    StreamParser: add empty fields + handle case where stream element spans multiple fields

--- src/main/scala/be/coiba/fixedlength/StreamParser.scala
+++ src/main/scala/be/coiba/fixedlength/StreamParser.scala
@@ -1,22 +1,29 @@
 package be.coiba.fixedlength
 
 import cats.effect.Sync
-import cats.syntax.functor._
+import cats.implicits._
 import fs2._
 
 object StreamParser {
   def parseRecord[F[_]: Sync](s: Stream[F, String], definition: RecordDefinition): F[Record] =
-    s.mapAccumulate(definition.fields -> "") {
-        case ((fieldDef :: fieldDefs, acc), in) if fieldDef.length <= acc.length + in.length =>
+    s.flatMap(str => Stream.emits(str.toCharArray()))
+      .mapAccumulate(definition.fields -> "") {
+        case ((fieldDef :: fieldDefs, acc), in) if fieldDef.length <= acc.length + 1 =>
           val (stringWithLength, rest) = (acc + in).splitAt(fieldDef.length)
           (fieldDefs, rest) -> Some(Field(fieldDef.name, stringWithLength))
-        case ((Nil, _), _) => (Nil, "") -> None
+        case ((Nil, _), _)          => (Nil, "") -> None
         case ((fieldDefs, acc), in) => (fieldDefs, acc + in) -> None
       }
       .map(_._2)
       .unNone
       .compile
       .toList
+      .map { parsedFields =>
+        parsedFields ++ definition.fields
+          .drop(parsedFields.size)
+          .map(fieldDef => Field(fieldDef.name, ""))
+      }
       .map(Record)
 
+  final case class ParserState(fieldDefs: List[FieldDefinition], rest: String, fields: List[Field])
 }
